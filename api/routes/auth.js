5f3110f9eecfa6b2a9320c7f203285b22f11e1c9
adnanali000 <adnan.museel123@gmail.com>
2021-08-04 00:13:17 +0500

    authentication work done

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -20,4 +20,23 @@ router.post("/register",async(req,res)=>{
     }
 })
 
+//login
+router.post("/login",async(req,res)=>{
+    try{
+        const user = await User.findOne({username: req.body.username});
+        !user && res.status(400).json("wrong credentials");
+
+        const validate = await bcrypt.compare(req.body.password,user.password);
+        !validate && res.status(400).json("wrong credentials");
+
+        const {password,...others} = user._doc;
+        res.status(200).json(others);
+
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
+
+
 module.exports = router;
