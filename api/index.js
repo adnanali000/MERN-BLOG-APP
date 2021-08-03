876e0ee7d3d9dd8af27a81f6faa309572043007a
adnanali000 <adnan.museel123@gmail.com>
2021-08-03 23:55:33 +0500

    post req successful and installed bcrypt to encrypt password

--- api/index.js
+++ api/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+//auth route
+const authRoute = require('./routes/auth');
 
 dotenv.config();
-
+app.use(express.json());
 
 //mongoose connection
 mongoose.connect(process.env.MONGO_URL, {
@@ -12,6 +14,9 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
     useCreateIndex:true,
 }).then(console.log("connected to mongoDB")).catch(err => console.log(err));
+
+//indicate router for authentication / login
+app.use("/api/auth",authRoute);
 
 
 //app listen on port 5000
