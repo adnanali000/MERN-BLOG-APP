771d250804e91ee49e84dfa7ba6e0ac702c0d0f5
adnanali000 <adnan.museel123@gmail.com>
2021-08-03 21:09:24 +0500

    connected to mongodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
+dotenv.config();
+
+
+//mongoose connection
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex:true,
+}).then(console.log("connected to mongoDB")).catch(err => console.log(err));
+
+
+//app listen on port 5000
 app.listen("5000",()=>{
     console.log("backend work begin")
 })
